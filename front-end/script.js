06cba956146cf7b5f7644dc95535055722aeb251
Aayush Arora <89267748+aayush12arora@users.noreply.github.com>
2023-09-23 00:21:43 +0530

    Update script.js

--- front-end/script.js
+++ front-end/script.js
@@ -26,55 +26,77 @@
 
 
 // Show the progress bar
-// Define an asynchronous function
+const progressBar = document.querySelector('.progress');
+progressBar.style.display = 'block';
 
 
+chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    var tab = tabs[0];
+    var tablink = tab.url;
+    const urlDisplay = document.getElementById("urlDisplay");
+    urlDisplay.innerHTML = tablink;
+    console.log(tablink);
 
+    const data = { url: tablink };
 
-async function fetchDataAndDisplay(url) {
-    const progressBar = document.querySelector('.progress');
-    progressBar.style.display = 'block';
+    // Send a POST request to your Flask endpoint
+    fetch('http://127.0.0.1:5000/process', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    })
+        .then(response => response.json())
+        .then(data => {
+            // Extract the prediction from the response
+            progressBar.style.display = 'none';
+            const prediction = data.prediction[0];
 
-    try {
-        const data = { url };
+            // Check if the prediction is 0 (phishing) or 1 (safe)
+            const isPhishing = prediction === 0;
+            const jokeelement = document.getElementById("jokeElement");
+            // Now, 'isPhishing' contains true if it's a phishing site, false if it's safe
+            console.log(isPhishing); 
+             if (isPhishing) {
+                jokeelement.classList.add("result-phishing");
+                jokeelement.textContent = "This site might be phishing!";
+            } else {
+                jokeelement.classList.add("result-safe");
+                jokeelement.textContent = "This site is safe!";
+            }
+            
 
-        // Send a POST request to your Flask endpoint
-        const response = await fetch('http://127.0.0.1:5000//process', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(data),
+            // You can use 'isPhishing' as needed within this function
+        })
+        .catch(error => {
+            console.error('Error:', error);
         });
-
-        const result = await response.json();
-
-        // Extract the prediction from the response
-        progressBar.style.display = 'none';
-        const prediction = result.prediction[0];
-
-        // Check if the prediction is 0 (phishing) or 1 (safe)
-        const isPhishing = prediction === 0;
-        const jokeelement = document.getElementById("jokeElement");
-        // Now, 'isPhishing' contains true if it's a phishing site, false if it's safe
-        console.log(isPhishing);
-        if (isPhishing) {
-            jokeelement.classList.add("result-phishing");
-            jokeelement.textContent = "This site might be phishing!";
-        } else {
-            jokeelement.classList.add("result-safe");
-            jokeelement.textContent = "This site is safe!";
-        }
-
-        // You can use 'isPhishing' as needed within this function
-    } catch (error) {
-        console.error('Error:', error);
-    }
-}
-
-chrome.tabs.onActivated.addListener(function (tabId) {
-    chrome.tabs.get(tabId.tabId, function (tab) {
-        const url = tab.url;
-        fetchDataAndDisplay(url);
-    });
 });
+
+
+
+
+
+
+
+// chrome.tabs.onActivated.addListener(function (tabId) {
+// var url;
+// var tab_id = tabId.tabId;
+// chrome.tabs.get(tab_id, function(tab){
+//     url = tab.url;
+// });
+
+// doStuff(url);
+// });
+
+// // Get the URL length and URL from the URL parameters
+// const params = new URLSearchParams(window.location.search);
+// const urlLength = params.get("urlLength");
+// const url = params.get("url");
+
+// // Display the URL length and URL in the popup
+// document.getElementById('url-length').textContent = `URL Length: ${urlLength}`;
+// document.getElementById('url-display').textContent = `URL: ${url}`;
+
+  
