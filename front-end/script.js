a0abad9dc2ab1bcb86ce2d09c53f7e53c36da0a4
Rahuldj <rj712@snu.edu.in>
2023-09-23 01:01:25 +0530

    CHROME-EXTENSION-INTEGRATED-AAYUSH

--- front-end/script.js
+++ front-end/script.js
@@ -1,36 +1,48 @@
-
-chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     var tab = tabs[0];
     var tablink = tab.url;
     const urlDisplay = document.getElementById("url-display");
-    urlDisplay.innerHTML= tablink;
-    console.log(tablink);
+    const progressContainer = document.querySelector(".progress-container");
+    const progress = document.querySelector(".progress");
 
-//--------------------------------------------------------------------------------------------
-const data = { url: tablink };
-console.log(data)
-// Send a POST request to your Flask endpoint
-fetch('http://127.0.0.1:5000/process', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),
-})
-.then(response => response.json())
-.then(res => {
-    console.log(res); // Log the response from Flask
-})
-.catch(error => {
-    console.error('Error:', error);
+    // Hide the result display initially
+    urlDisplay.style.display = "none";
+
+    // Show the progress bar
+    progressContainer.style.display = "block";
+
+    // Create an object with the URL
+    const data = { url: tablink };
+
+    // Send a POST request to your Flask endpoint
+    fetch('http://127.0.0.1:5000/process', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    })
+    .then(response => response.json())
+    .then(res => {
+        // Hide the progress bar
+        progressContainer.style.display = "none";
+
+        // Display the result
+        urlDisplay.style.display = "block";
+
+        // Access the "prediction" array and use its data
+        if (res && res.prediction && res.prediction.length > 0) {
+            const predictionValue = res.prediction[0];
+            if (predictionValue == 0) {
+                urlDisplay.innerHTML = "Suspicious Site!! Be Careful!! ";
+                urlDisplay.classList.add("result-phishing");
+            } else if (predictionValue == 1) {
+                urlDisplay.innerHTML = "Good to go";
+                urlDisplay.classList.add("result-safe");
+            }
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
 });
-
-
-
-
-   
-  });
-
-
-
-  
